bfd4841bc0541a4245d4b3d492f58efbea77a0e8
Oleg Selivantsev <o.selivancev@gmail.com>
2025-03-06 00:52:15 +0400

    items added

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+def get_fruits():
+    items = ["item1", "item2", "item3"]
+    for item in items:
+        # print(fruit)
+        return items
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": get_fruits()}
