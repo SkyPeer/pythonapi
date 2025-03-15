191500dc1894e8559dc487186f405f6dbf13013b
Oleg Selivantsev <o.selivancev@gmail.com>
2025-03-16 00:12:29 +0400

    class data added

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ app = FastAPI()
 
 # toStart uvicorn main:app --reload --port 8000
 
+class Data:
+    def __init__(self, items):
+        self.items = items
+
+    def get_items_with_offset(self, offset):
+        items_length = len(self.items)
+        offset = slice(offset, items_length)
+        return self.items[offset]
+
 def get_fruits():
     items = ["item1", "item2", "item3"]
     for item in items:
@@ -13,3 +22,8 @@ def get_fruits():
 @app.get("/")
 async def root():
     return {"message": get_fruits()}
+
+@app.get("/data")
+async def items():
+    data = Data(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+    return data.get_items_with_offset(3)
