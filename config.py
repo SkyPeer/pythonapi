114b55e8f751a39d8ab87552d4f83100c0f90b9d
Oleg Selivantsev <o.selivancev@gmail.com>
2025-03-24 01:41:51 +0400

    config bd added

---
+++ config.py
@@ -0,0 +1,14 @@
+config_bd = '12323123'
+secret_key = '12323123'
+
+# database=database_config["database"], user="", password="pass", host="localhost", port=5432
+
+database_config = {
+   "database": "streamway",
+    "user": "",
+    "password": "",
+    "host": "localhost",
+    "port": 5432
+
+    # user="", password="pass", host="localhost", port=5432
+}
--- main.py
+++ main.py
@@ -4,15 +4,30 @@ from fastapi import FastAPI, Request
 from data import Data
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 import psycopg2
+from config import database_config
 
-connection = psycopg2.connect(database="streamway", user="", password="pass", host="localhost", port=5432)
+connection = psycopg2.connect(
+    database=database_config["database"],
+    user=database_config["user"],
+    password=database_config["password"],
+    host=database_config["host"],
+    port=database_config["port"])
 
+cursor = connection.cursor()
 
 print('__name__', __name__)
 data = Data()
 app = FastAPI()
 
 # toStart uvicorn main:app --reload --port 8000
+
+def get_data() -> None:
+    cursor.execute(""" SELECT id, slug, title, description, body, "tagList", "favoritesCount" FROM public.articles; """)
+    data = cursor.fetchall()
+    # data = cursor.fetchone()
+    return data
+
+
 
 @app.get("/")
 async def root():
@@ -23,7 +38,7 @@ async def items():
     random_timeout = random.randint(1, 4)
     print('random_timeout', random_timeout)
     # time.sleep(random_timeout)
-    return {"timeout": random_timeout, "data": data.get_items_with_offset(random_timeout)}
+    return {"timeout": random_timeout, "data": data.get_items_with_offset(random_timeout), "articles": get_data()}
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
@@ -31,4 +46,5 @@ async def add_process_time_header(request: Request, call_next):
     response = await call_next(request)
     process_time = time.perf_counter() - start_time
     response.headers["X-Process-Time"] = str(process_time)
+    response.headers["New-header"] = "test"
     return response
